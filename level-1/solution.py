4b9cfb28bb6c12b66e32c8cf9c150069a4221f60
herosunly <liuyang@mail.bnu.edu.cn>
2018-07-09 14:45:50 +0800

    modify the error

--- level-1/solution.py
+++ level-1/solution.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 import string
 
-in_tab = string.ascii_letters
-out_tab = string.ascii_letters[2:] + string.ascii_letters[:2]
+in_tab = string.ascii_lowercase
+out_tab = string.ascii_lowercase[2:] + string.ascii_lowercase[:2]
 tran_tab = string.maketrans(in_tab, out_tab)
 result = 'map'.translate(tran_tab)
 print result
